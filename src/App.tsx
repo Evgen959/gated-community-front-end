a778ab8d22da733db4994bdb0852a92d8e4a1fe5
Evgen Grushchenko <evgen.tm959@gmail.com>
2024-10-22 17:43:56 +0200

    Evgen012

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import GatedCommunity from './pages/GatedCommunity';
 
 function App() {
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <GatedCommunity />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
